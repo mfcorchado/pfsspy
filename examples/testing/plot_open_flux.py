6f6ec9a969e51276b2a0c046b1679ab16cbd798e
David Stansby <dstansby@gmail.com>
2021-05-20 16:19:08 +0100

    Update open flux code

--- examples/testing/plot_open_flux.py
+++ examples/testing/plot_open_flux.py
@@ -21,6 +21,7 @@ import numpy as np
 import scipy.integrate
 import sunpy.map
 import sympy
+import pandas as pd
 import pfsspy
 import pfsspy.coords as coords
 from pfsspy import analytic
@@ -47,19 +48,39 @@ def open_flux_numeric(l, m, zss, nrho):
 zss = 2
 
 fig, ax = plt.subplots()
-lm = [[1, 0], [2, 0], [3, 0]]
-for l, m in lm:
-    print(l, m)
+lms = [10, 11, 20, 21, 22, 30]
+
+# nrhos = np.array([5, 50])
+nrhos = np.arange(10, 51, 2)
+
+df = pd.DataFrame(index=nrhos, columns=lms)
+
+for lm in lms:
+    l = lm // 10
+    m = lm % 10
     flux_analytic = open_flux_analytic(l, m, zss)
     flux_numeric = []
-    nrhos = np.array([5, 10, 20, 50])
-    nrhos = np.arange(10, 51, 2)
     flux_numeric = np.array([open_flux_numeric(l, m, zss, nrho) for nrho in nrhos])
-    ax.plot(nrhos, flux_numeric / flux_analytic, marker='o', label=f'l={l}, m={m}')
+    flux_ratio = flux_numeric / flux_analytic
+    df[lm] = flux_ratio
+
+df.to_csv('results.csv')
+
+df = pd.read_csv('results.csv', index_col=0)
+
+for lm in lms:
+    l = lm // 10
+    m = lm % 10
+    color = {1: 'tab:blue', 2: 'tab:orange', 3: 'tab:green'}[l]
+    marker = {0: 'o', 1: 10, 2: 11}[m]
+    ax.plot(nrhos, df[str(lm)],
+            marker=marker, color=color,
+            label=f'l={l}, m={m}')
 
 ax.legend()
 ax.set_ylim(1)
-ax.set_xlim(0)
+ax.set_xlim(8)
+ax.yaxis.grid(linestyle='--')
 ax.set_xlabel('$n_{r}$')
 ax.set_ylabel('$\Phi_{pfsspy} / \Phi_{analytic}$')
 plt.show()
